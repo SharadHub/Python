178cf225e332ad309fdb9cc4da2f821d1278b349
Sharad Bista <124153048+SharadHub@users.noreply.github.com>
2025-03-26 23:53:59 +0545

    Add files via upload
    These files contains basic of python programming language mostly I have covered if/else statement. I learn how to use if, elif, else, not in, is, short hand if...else, etc.

---
+++ revised/basic of python.py
@@ -0,0 +1,46 @@
+#datatypes of python
+#1. int
+a = 10 
+print(type(a))
+
+
+#2. float
+a = 10.0
+print(type(a))
+
+
+#3. string
+a = "10"
+print(type(a))
+
+
+#4. list
+a = [1,2,3]
+print(type(a))
+
+
+#5. tuple
+a = (1,2,3)
+print(type(a))
+
+
+#6. dictionary
+print({"name": "bishal", "roll": 21, "class": "bca"})
+print(type(a))
+
+
+#7. set
+a = {1,2,3}
+print(type(a))
+
+
+#8. bool
+a = 10
+b = 20
+print(a>b)
+
+
+#9. complex
+a = 10 + 20j
+print(a)
+print(type(a))
